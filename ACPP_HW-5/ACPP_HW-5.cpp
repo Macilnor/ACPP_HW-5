b39aae58c11a9f2bea59426daf474e0ca77477b9
Macilnor <macilnor@mail.ru>
2021-09-20 16:56:41 +0300

    Задание 2

--- ACPP_HW-5/ACPP_HW-5.cpp
+++ ACPP_HW-5/ACPP_HW-5.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include <list>
+#include <map>
 #include <vector>
+#include <algorithm>
 #include "UniqueWords.h"
 
 int main()
@@ -19,6 +21,8 @@ int main()
 									  "Die", "by", "any", "means", "necessary" };
 	UniqueWords(v_words.begin(), v_words.end());
 
+	std::cout << std::endl;
+
 	std::list<std::string> l_words{ "I", "am", "not", "judgemental",
 									"A", "sinner", "nor", "a", "saint",
 									"Cause", "either", "you're", "my", "best", "friend", "or", "you", "ain't",
@@ -33,6 +37,31 @@ int main()
 									"Die", "by", "any", "means", "necessary" };
 	UniqueWords(l_words.begin(), l_words.end());
 
+	std::cout << std::endl;
 
+	std::cout << std::endl << "Input 5 sentences. Sentence must contain '.' at the end." << std::endl;
+	std::string input;
+	std::map<size_t, std::string> sentences;
+	while ((std::getline(std::cin, input, '.')) && (sentences.size() < 5))
+	{
+		for (auto it = input.begin(); it != input.end(); ++it)
+		{
+			if (*it == '\n')
+			{
+				input.erase(it);
+				if (it == input.end())
+					break;
+			}
+		}
+		if (!input.empty())
+		{
+			sentences.insert({ input.length(), input });
+		}
+
+	}
+	std::cout << std::endl;
+	std::for_each(sentences.begin(), sentences.end(),
+		[](std::pair<size_t, std::string> item) {
+			std::cout << item.first << " " << item.second << std::endl; });
 }
 
--- ACPP_HW-5/UniqueWords.h
+++ ACPP_HW-5/UniqueWords.h
@@ -8,5 +8,5 @@ void UniqueWords(iterator begin, iterator end)
 {
 	std::set<std::string> uniqueWords;
 	std::copy(begin, end, std::inserter(uniqueWords, uniqueWords.begin()));
-	std::copy(uniqueWords.begin(), uniqueWords.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
+	std::copy(uniqueWords.begin(), uniqueWords.end(), std::ostream_iterator<std::string>(std::cout, " "));
 }
